d5382a370305822498ca5086f6dbe60f5a450db9
HiroUrata <1712026@i.softbank.jp>
2021-07-17 18:24:31 +0900

    Initial Commit

---
+++ UISliderLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISliderLesson
+//
+//  Created by UrataHiroki on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
